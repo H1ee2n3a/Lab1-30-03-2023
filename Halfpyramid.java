54bb5f1566e455967327b247aa5d7788040b91d9
Heenakousar Nadaf <104358429+H1ee2n3a@users.noreply.github.com>
2023-03-31 13:02:35 +0530

    Create Halfpyramid.java

---
+++ Halfpyramid.java
@@ -0,0 +1,20 @@
+package basic;
+
+public class Halfpyramid {
+
+	public static void main(String[] args) {
+	int i,j;
+	for(i=1;i<=7;i++)
+	{
+		for(j=1;j<=i;j++) {
+			System.out.print("#");
+			
+			}
+		System.out.println();
+		
+		
+	}
+
+	}
+
+}
