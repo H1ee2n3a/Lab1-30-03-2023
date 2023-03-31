1178b6544b14bdd2546e25678f6d354a6647902a
Heenakousar Nadaf <104358429+H1ee2n3a@users.noreply.github.com>
2023-03-31 13:01:38 +0530

    Create Inverted_Halfpyramid.java

---
+++ Inverted_Halfpyramid.java
@@ -0,0 +1,17 @@
+package basic;
+
+public class Inverted_Halfpyramid {
+
+	public static void main(String[] args) {
+		int i,j;
+		for(i=7;i>=1;i--)
+		{
+			for(j=i;j>=1;j--)
+			{
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+
+	}
+}
